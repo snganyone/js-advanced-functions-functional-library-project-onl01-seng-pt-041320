4c540cd5edbe117d0cca83b4c4b6ecaa14f18844
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-18 20:01:51 -0800

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for(let x = 0; x < collection.length; x++){
+        callback(collection[x]);
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let arr = [];
+      for(let x = 0; x < collection.length; x++){
+        let y = callback(collection[x]);
+        arr.push(y);
+      }
+      return arr;
     },
 
     reduce: function() {
